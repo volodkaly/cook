380b924c7a6ca103a7108531a94908628f454de1
volodkaly <lysvl@hotmail.cz>
2022-11-03 14:54:09 +0100

    good

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -15,9 +15,16 @@
         <h1 class="my-3 text-center">Receipts</h1>
         <h2>{{ $rec->title }}</h2>
         <p>{{ $rec->content }}</p>
-        <a href="{{ route('rec.edit', ['rec' => $rec->id]) }}">Edit</a>
-        <a href="{{ route('rec.delete', ['rec' => $rec->id]) }}">Delete</a>
-        <p><a href="{{ route('index') }}">To the list of Reciepts</a></p>
+        <a href="{{ route('rec.edit', ['rec' => $rec->id]) }}">
+        <button type="button" class="btn btn-primary">Edit
+        </button>
+        </a>
+        <a href="{{ route('rec.delete', ['rec' => $rec->id]) }}">
+        <button type="button" class="btn btn-danger">
+            Delete
+        </button>
+        </a>
+        <p><a  href="{{ route('index') }}" class="text-center">To the list of Reciepts</a></p>
     </div>
 </body>
 
